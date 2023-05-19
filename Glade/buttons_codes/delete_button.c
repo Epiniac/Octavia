a665830fe7904b02ed22afc3baf9f5b3b113b41b
Anne-laure <anne-laure.aissi@epita.fr>
2023-05-19 20:43:08 +0200

    det

--- Glade/buttons_codes/delete_button.c
+++ Glade/buttons_codes/delete_button.c
@@ -1,27 +1,28 @@
 #include <gtk/gtk.h>
 
 // Callback function to be called when the button is clicked
-void on_button_clicked(GtkButton button, gpointer user_data) {
+void delete_image(GtkButton button, gpointer user_data) {
     g_print("Button clicked!\n");
+    
 }
 
-int main(int argc, charargv[]) {
-    GtkWidget window;
-    GtkBuilderbuilder;
+int main(int argc, char **argv)
+{
+    GtkWidget *window;
+    GtkBuilder *builder;
     GObject *button;
 
     // Initialize GTK
     gtk_init(&argc, &argv);
 
     // Load the Glade file
-    builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "../editor_glade.glade", NULL);
+    builder = gtk_builder_new_from_file("../editor.glade");
 
     // Get a reference to the button widget
     button = gtk_builder_get_object(builder, "delete_button");
 
     // Connect the button's "clicked" signal to the callback function
-    g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
+    g_signal_connect(button, "clicked", G_CALLBACK(delete_image), NULL);
 
     // Create the window and set its properties
     window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
